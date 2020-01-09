0a86a50ea00ce0731d534f96ba3ca1792684d306
Isvvc <lyons@tuta.io>
2020-01-09 15:32:48 -0700

    Create unit test target

---
+++ Portfolio/PortfolioTests/PortfolioTests.swift
@@ -0,0 +1,33 @@
+//
+//  PortfolioTests.swift
+//  PortfolioTests
+//
+//  Created by Isaac Lyons on 1/9/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import XCTest
+
+class PortfolioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
