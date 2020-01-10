387b7e36d0e8ff17ef31798aab5f61896c79f87a
Isvvc <lyons@tuta.io>
2020-01-09 17:58:56 -0700

    Create UI Test target

---
+++ Portfolio/PortfolioUITests/PortfolioUITests.swift
@@ -0,0 +1,13 @@
+//
+//  PortfolioUITests.swift
+//  PortfolioUITests
+//
+//  Created by Isaac Lyons on 1/9/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import XCTest
+
+class PortfolioUITests: XCTestCase {
+
+}
